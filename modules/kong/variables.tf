706f5ea4936413168612b4691d9a1a66991a7ec6
Jyojith George Thaliath <jyojith@gmail.com>
2025-06-25 17:00:28 +0200

    make other changes as well

--- modules/kong/variables.tf
+++ modules/kong/variables.tf
@@ -1,8 +1,3 @@
-variable "reserved_ip" {
-  description = "Shared reserved IP for ArgoCD LoadBalancer"
-  type        = string
-}
-
 variable "domain_name" {
   description = "Domain name for routing"
   type        = string
