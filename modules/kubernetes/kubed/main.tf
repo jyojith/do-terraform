ed25f32398625504e9e55bef2aa926421439153a
Jyojith George Thaliath <jyojith@gmail.com>
2025-06-26 00:32:24 +0200

    further canges for certificate

--- modules/kubernetes/kubed/main.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "kubed" {
-  name             = "kubed"
-  repository       = "https://charts.appscode.com/stable/"
-  chart            = "kubed"
-  version          = "v0.12.0"
-  namespace        = "kube-system"
-  create_namespace = true
-}
