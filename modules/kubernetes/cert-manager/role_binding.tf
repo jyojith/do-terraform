714f09b793b65075074af747d5121b0acd796ad5
Jyojith George Thaliath <jyojith@gmail.com>
2025-06-26 12:08:04 +0200

    fmt

--- modules/kubernetes/cert-manager/role_binding.tf
+++ modules/kubernetes/cert-manager/role_binding.tf
@@ -11,8 +11,8 @@ resource "kubernetes_role_binding" "sync_secret_access" {
   }
 
   role_ref {
-    kind     = "Role"
-    name     = kubernetes_role.read_certs.metadata[0].name
+    kind      = "Role"
+    name      = kubernetes_role.read_certs.metadata[0].name
     api_group = "rbac.authorization.k8s.io"
   }
 }
